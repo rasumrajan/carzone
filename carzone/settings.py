fdd7aca528816b16c40af48bbdccd4f49fbede8c
rasumrajan <coolrasum@gmail.com>
2022-04-25 17:45:50 +0530

    checking

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '934nw3r62@!m0^ksgw3#31tntglnr%td+-_b89xpu2@q2zqv=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
